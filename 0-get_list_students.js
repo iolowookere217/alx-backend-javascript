93275c85fa743d6100ae20523d65319fd974a288
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-04 03:57:40 +0000

    task o

---
+++ 0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents () {
+  const firstObject = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco'
+  };
+
+  const secondOject = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia'
+  };
+
+  const thirdObject = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco'
+  };
+
+  return [firstObject, secondObject, thirdObject];
+}
