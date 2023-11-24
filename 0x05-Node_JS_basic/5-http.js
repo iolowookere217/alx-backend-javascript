26107b57f2b7fbd0f0ebe486d01f7b870c3b299c
iolowookere217 <olowookere.isaaco@gmail.com>
2023-11-24 03:06:49 +0000

    Task 0

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -26,15 +26,13 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
         const dbFieldNames = fileLines[0].split(',');
         const studentPropNames = dbFieldNames.slice(
           0,
-          dbFieldNames.length - 1
-        );
+          dbFieldNames.length - 1);
 
         for (const line of fileLines.slice(1)) {
           const studentRecord = line.split(',');
           const studentPropValues = studentRecord.slice(
             0,
-            studentRecord.length - 1
-          );
+            studentRecord.length - 1);
           const field = studentRecord[studentRecord.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
             studentGroups[field] = [];
